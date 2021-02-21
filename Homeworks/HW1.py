5e84a52356bb666488b888c3c98d7c539ed4b678
fazliii <78759280+fazliii@users.noreply.github.com>
2021-02-21 22:08:16 +0300

    Update HW1.py

--- Homeworks/HW1.py
+++ Homeworks/HW1.py
@@ -1,5 +1,19 @@
-#Explain your work
+import random as rnd
 
-#Question 1
-for x in range(a):
-	print(a)
+alt = 1 
+ust = 100
+liste=[]
+
+for sayi in range(alt,ust+1):  
+    #Alt ve üst değerlerimin arasındaki sayıları kontrol ettirdim.
+   if sayi > 1:  
+       for i in range(2,sayi):  
+           if (sayi % i) == 0:  
+               break  
+       else:               
+                  liste.append(sayi) #Asal sayıları listeme ekledim
+
+for i in range(1):        
+  for j in range(3):
+     print("      ",rnd.sample(set(liste), 3)) 
+   #Random değerleri birbirinden farklı olacak şekilde liste olarak yazdırdım.   
